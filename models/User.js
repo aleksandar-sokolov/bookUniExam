b916fb9d3a15fa3af1e36c33e7718cd43e2db078
Aleksandar <aleksandar-sokolov@github.com>
2021-02-16 12:40:30 +0200

    Add user schema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const moongose = require('mongoose');
+
+const userShema = new moongose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bookedHotels: [{
+        type: moongose.Types.ObjectId,
+        ref: 'Hotel'
+    }],
+    offeredHotels: [{
+        type: moongose.Types.ObjectId,
+        ref: 'Hotel'
+    }],
+
+});
+
+module.exports = moongose.model('User', userSchema); 
